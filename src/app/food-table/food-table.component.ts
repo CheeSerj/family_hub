7b64e6721f29a4359b31a869ef0e0fb6a6e67501
CheeSerj <chibisovse@yandex.ru>
2023-05-08 23:05:59 +0300

    Редизайн кашевара

--- src/app/food-table/food-table.component.ts
+++ src/app/food-table/food-table.component.ts
@@ -3,9 +3,7 @@ import {Router} from "@angular/router";
 
 export interface Table{
   water: string;
-  inPack: string;
-  iDo: string;
-  countWater: string;
+  blend: string;
 }
 
 @Component({
@@ -17,13 +15,12 @@ export interface Table{
 export class FoodTableComponent implements OnInit, OnDestroy {
 
   public table: Table[] = [
-    {water: '30гр', countWater: '30гр', inPack: '4,4гр', iDo: '5гр'},
-    {water: '60гр', countWater: '60гр', inPack: '8,8гр', iDo: '9гр'},
-    {water: '90гр', countWater: '90гр', inPack: '13,2гр', iDo: '13гр'},
-    {water: '120гр', countWater: '120гр', inPack: '17,6гр', iDo: '18гр'},
-    {water: '160гр', countWater: '160гр', inPack: '22,0гр', iDo: '22гр'},
-    {water: '160гр', countWater: '190гр', inPack: '26,4гр', iDo: '27гр'},
-    {water: '180гр', countWater: '210гр', inPack: '30,8гр', iDo: '32гр'}
+    {water: '30гр',  blend: '5гр'},
+    {water: '60гр',  blend: '9гр'},
+    {water: '90гр',  blend: '13гр'},
+    {water: '120гр', blend: '18гр'},
+    {water: '160гр', blend: '27гр'},
+    {water: '180гр', blend: '32гр'}
   ]
 
   constructor(public router: Router) { }
