6dc19429c1c694b73f63989682cc1920d798dab4
CheeSerj <chibisovse@yandex.ru>
2023-06-10 14:11:47 +0300

    rename app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/calculate-timer'),
+      dir: require('path').join(__dirname, './coverage/family_hub'),
       subdir: '.',
       reporters: [
         { type: 'html' },
